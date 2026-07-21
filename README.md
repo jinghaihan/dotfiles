ec4ca0e5642d7a977ba190abc2d0b8b03a21d33c
jinghaihan <jhh19980114@gmail.com>
2026-07-21 22:11:50 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<samp><b>Dotfiles</b></samp>
+
+- [.zshrc](./.zshrc)
+
+<samp><b>Check Also</b></samp>
+
+- [dotfiles](https://github.com/jinghaihan/dotfiles) - My dotfiles
+- [vscode-settings](https://github.com/jinghiahan/vscode-settings) - My VS Code settings
---
+++ brew-install.sh
@@ -0,0 +1,52 @@
+# system & productivity
+brew install --cask google-chrome
+brew install --cask raycast
+brew install --cask jordanbaird-ice
+brew install --cask rectangle
+brew install --cask openinterminal
+brew install --cask itsycal
+brew install --cask battery-buddy
+brew install --cask mos
+brew install --cask input-source-pro
+
+# media & file tools
+brew install --cask pictureview
+brew install --cask iina
+brew install --cask kap
+brew install --cask the-unarchiver
+brew install --cask keka
+
+# communication
+brew install --cask discord
+brew install --cask telegram
+brew install --cask wechat
+brew install --cask tencent-meeting
+
+# entertainment
+brew install --cask spotify
+
+# notes & office
+brew install --cask obsidian
+brew install --cask wpsoffice
+brew install --cask nutstore
+
+# system utilities
+brew install --cask tencent-lemon
+
+# notch
+brew install --cask topnotch
+brew install --cask theboredteam/boring-notch/boring-notch
+
+# development
+brew install --cask ghostty
+brew install --cask tabby
+brew install --cask visual-studio-code
+brew install --cask figma
+brew install --cask beekeeper-studio
+brew install --cask orbstack
+
+# ai tools
+brew install --cask codex
+brew install --cask codex-app
+brew install --cask claude-code
+brew install --cask codexbar
